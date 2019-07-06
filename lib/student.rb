ead660b1ae9205cdd4fa64f62096938c957d28e7
Abagail Hudson <abagailanne@gmail.com>
2019-07-06 22:59:21 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ class Student
   attr_reader :id
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
- def initialize(id)
+ def initialize(id = nil)
    @id = id
 end
 end
